67041a60f23a71040e9341f63f71369533448d95
pradal <christophe.pradal@inria.fr>
2018-03-16 22:27:13 +0100

    Update

--- test/notebook/test.py
+++ test/notebook/test.py
@@ -3,19 +3,30 @@ from openalea.mtg.aml import *
 
 from openalea import groalea
 from openalea.groalea.mappletConverter import convert
-from openalea.groalea.graphio import graph2xml, xml2graph, xmlFile2graph
+from openalea.groalea.graphio import (graph2xml, xml2graph,
+                                      xmlFile2graph, getSceneXEG,
+                                      getMTGRootedGraph)
 
 from openalea.deploy import shared_data
 from path import Path
+import time
 
 data_dir = Path('../../share/interface-example/')
 sti_fn, sti_fs_fn, st_fn = data_dir.glob('*.xeg')
 
 from openalea.groalea.topology import spanning_mtg
 
-for filename in (sti_fn, sti_fs_fn, st_fn):
-    graph, scene = xmlFile2graph(filename)
+scenes = []
+for filename in (sti_fn, st_fn):
+    graph, scene = xmlFile2graph(filename, True)
+    xeg_graph = getSceneXEG(graph)
+    _g, scene = xml2graph(xeg_graph)
+    scenes.append(scene)
+
+    graph, scene = xmlFile2graph(filename, True)
+    graph = getMTGRootedGraph(graph)
     g = spanning_mtg(graph)
+
+    print g.display()
     #g.display()
 
-#Viewer.display(scene)
